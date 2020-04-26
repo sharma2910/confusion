ad13a3e6a34a1815db1dbb9883333827a913d33e
Darshit Sharma <darshit.sharma29@gmail.com>
2020-04-26 19:04:16 +0530

    Redux part 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Card, CardBody, CardText, CardTitle, CardImg } from 'reactstrap';
-import { Link } from 'react-router-dom'
 
-function RenderComment({comments}){
-    if(comments != null){
-        let com = comments.map((comment) => {
-            return (
-                <div key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>--  {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                </div>
-            )
-        })
-    }else{
-        return(
-            <div></div>
-        );
-    }
-}
+// function RenderComment({comments}){
+//     if(comments != null){
+//         let com = comments.map((comment) => {
+//             return (
+//                 <div key={comment.id}>
+//                     <p>{comment.comment}</p>
+//                     <p>--  {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+//                 </div>
+//             )
+//         })
+//     }else{
+//         return(
+//             <div></div>
+//         );
+//     }
+// }
 
 function RenderDish({dish}) {
     if (dish != null) {
